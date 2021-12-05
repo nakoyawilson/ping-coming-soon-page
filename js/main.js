c0f5dfb881e6d34cbd9069dfc63e4c957fdb1462
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-12-05 15:24:22 -0400

    Change error message depending on if blank or invalid

--- js/main.js
+++ js/main.js
@@ -5,13 +5,13 @@ submitButton.addEventListener("click", (e) => {
   const emailInput = inputField.value;
   const errorMessage = document.querySelector(".error-message");
   const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  if (
-    emailInput === "" ||
-    emailInput === null ||
-    !emailInput.match(emailRegex)
-  ) {
+  if (emailInput === "" || emailInput === null) {
     errorMessage.style.display = "block";
     inputField.style.borderColor = "hsl(354, 100%, 66%)";
+    errorMessage.innerHTML =
+      "Whoops! It looks like you forgot to add your email";
+  } else if (!emailInput.match(emailRegex)) {
+    errorMessage.innerHTML = "Please provide a valid email address";
   } else {
     errorMessage.style.display = "none";
     inputField.style.borderColor = "hsl(223, 100%, 88%)";
