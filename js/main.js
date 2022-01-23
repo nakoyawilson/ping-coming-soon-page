043981268e246975896c53efcd197d501380952d
Nakoya Wilson <wilson.nakoya@gmail.com>
2022-01-23 07:51:04 -0400

    Improve and fix form validation

--- js/main.js
+++ js/main.js
@@ -1,20 +1,29 @@
-const submitButton = document.querySelector(".notify");
+const notifyForm = document.querySelector("form");
 const inputField = document.querySelector("#email");
 
-submitButton.addEventListener("click", (e) => {
+notifyForm.addEventListener("submit", (e) => {
   const emailInput = inputField.value;
   const errorMessage = document.querySelector(".error-message");
   const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
   if (emailInput === "" || emailInput === null) {
     errorMessage.style.display = "block";
-    inputField.style.borderColor = "hsl(354, 100%, 66%)";
+    inputField.classList.add("error-border");
     errorMessage.innerHTML =
       "Whoops! It looks like you forgot to add your email";
+    inputField.setAttribute("aria-invalid", "true");
+    inputField.setAttribute("aria-describedBy", "error");
   } else if (!emailInput.match(emailRegex)) {
+    errorMessage.style.display = "block";
+    inputField.classList.add("error-border");
     errorMessage.innerHTML = "Please provide a valid email address";
+    inputField.setAttribute("aria-invalid", "true");
+    inputField.setAttribute("aria-describedBy", "error");
   } else {
     errorMessage.style.display = "none";
-    inputField.style.borderColor = "hsl(223, 100%, 88%)";
+    inputField.classList.remove("error-border");
+    inputField.removeAttribute("aria-invalid");
+    inputField.removeAttribute("aria-describedBy");
+    inputField.value = "";
   }
   e.preventDefault();
 });
